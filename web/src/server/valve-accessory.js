f66a3dd7eee9518cd129f88942fdbc280d6fef19
Glenn <mr.sharpoblunto@gmail.com>
2020-05-10 20:50:22 -0700

    Fixed homekit valve actions hanging

--- web/src/server/valve-accessory.js
+++ web/src/server/valve-accessory.js
@@ -4,45 +4,50 @@
 import * as hap from 'hap-nodejs';
 import * as config from './config';
 
-export default function(valveController) {
-    const valveUUID = hap.uuid.generate('hap-nodejs:accessories:valve');
+export default function (valveController) {
+  const valveUUID = hap.uuid.generate('hap-nodejs:accessories:valve');
 
-    const valve = new hap.Accessory('Water valve', valveUUID);
+  const valve = new hap.Accessory('Water valve', valveUUID);
 
-    valve
-        .getService(hap.Service.AccessoryInformation)
-        .setCharacteristic(hap.Characteristic.Manufacturer, config.MANUFACTURER)
-        .setCharacteristic(hap.Characteristic.Model, config.MODEL)
-        .setCharacteristic(hap.Characteristic.SerialNumber, config.SERIAL);
+  valve
+    .getService(hap.Service.AccessoryInformation)
+    .setCharacteristic(hap.Characteristic.Manufacturer, config.MANUFACTURER)
+    .setCharacteristic(hap.Characteristic.Model, config.MODEL)
+    .setCharacteristic(hap.Characteristic.SerialNumber, config.SERIAL);
 
-    valve.on('identify', function(paired, cb) {
-        valveController.identify();
-        cb();
+  valve.on('identify', function (paired, cb) {
+    valveController.identify();
+    cb();
+  });
+
+  valve
+    .addService(hap.Service.Switch, 'It gets the hose again')
+    .getCharacteristic(hap.Characteristic.On)
+    .on('set', function (value, cb) {
+      valveController
+        .setOpen(!!value, config.HOMEKIT_USER)
+        .then(() => {
+          cb();
+        })
+        .catch(err => {
+          cb(err);
+        });
     });
 
-    valve
-        .addService(hap.Service.Switch, 'It gets the hose again')
-        .getCharacteristic(hap.Characteristic.On)
-        .on('set', function(value, cb) {
-            valveController.setOpen(!!value, config.HOMEKIT_USER, cb);
-        });
+  valve
+    .getService(hap.Service.Switch)
+    .getCharacteristic(hap.Characteristic.On)
+    .on('get', function (cb) {
+      cb(valveController.getOpen() ? 1 : 0);
+    });
 
-    valve
+  valveController.on('setOpen', function ({open, source}) {
+    if (source !== config.HOMEKIT_USER) {
+      valve
         .getService(hap.Service.Switch)
-        .getCharacteristic(hap.Characteristic.On)
-        .on('get', function(cb) {
-            cb(valveController.getOpen() ? 1 : 0);
-        });
+        .setCharacteristic(hap.Characteristic.On, open ? 1 : 0);
+    }
+  });
 
-    valveController.on('setOpen', function({
-        open, source
-    }) {
-        if (source !== config.HOMEKIT_USER) {
-            valve
-                .getService(hap.Service.Switch)
-                .setCharacteristic(hap.Characteristic.On, open ? 1 : 0);
-        }
-    });
-
-    return valve;
+  return valve;
 }
